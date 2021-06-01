88db4626b37ae399c78680febcc992844ef0508a
Aakash Garg <aakashgarg2800@gmail.com>
2021-06-01 20:10:47 +0530

    Rooms and waiting area implement

--- contact/src/components/RoomPlayer.js
+++ contact/src/components/RoomPlayer.js
@@ -9,23 +9,72 @@ export default function RoomPlayer({ socket }) {
     });
   });
   useEffect(() => {
-    socket.once("player-left", gameData => {
+    socket.once("players-update", (gameData) => {
       console.log(gameData);
       setGameData(gameData);
-    })
+    });
   });
+  useEffect(() => {
+    socket.once("player-is-kicked", () => {
+      alert("you were kicked");
+      setGameData({});
+    });
+  });
+  function leaveRoom() {
+    socket.emit("player-left", () => {
+      alert("left successfully");
+      setGameData({});
+    });
+  }
+  function kickPlayer(playerId) {
+    socket.emit("kick-player", playerId);
+  }
+
+  function makeGameMaster(playerId) {
+    socket.emit("make-gamemaster", playerId);
+  }
+
   return (
     <div>
       <h1>Room Code</h1>
       <div>{gameData.roomCode}</div>
-      <h2>Players</h2> 
+      <h2>Players</h2>
       <div>
         {gameData.players &&
           gameData.players.map((player, key) => {
-            return <p key={key}>{player.name}</p>;
+            return (
+              <div key={key}>
+                <p key={key}>
+                  {player.name}
+                  {key === 0 ? "(host)" : null}
+                  {gameData.gameMasterId === player.id ? "(game master)" : null}
+                </p>
+                {gameData.players[0].id === socket.id && key !== 0 ? (
+                  <button
+                    onClick={() => {
+                      // alert(`The player : ${player.id}`);
+                      kickPlayer(player.id);
+                    }}
+                  >
+                    kick this bitch
+                  </button>
+                ) : null}
+                {gameData.players[0].id === socket.id &&
+                gameData.gameMasterId !== player.id ? (
+                  <button
+                    onClick={() => {
+                      makeGameMaster(player.id);
+                    }}
+                  >
+                    Make gameMaster{" "}
+                  </button>
+                ) : null}
+              </div>
+            );
           })}
       </div>
       <button>Waiting for host to start the game</button>
+      <button onClick={leaveRoom}>Leave Room</button>
     </div>
   );
 }
